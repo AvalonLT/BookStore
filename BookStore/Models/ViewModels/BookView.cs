28fd9459f90bbbb0296597626005b1659e1e333f
JONAS-PC\Jonas <100jonas@gmail.com>
2018-08-17 04:21:23 +0300

    DropDownList for Language

---
+++ BookStore/Models/ViewModels/BookView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models.ViewModels
+{
+    public class BookView
+    {
+        public Book Book { get; set; }
+        public Language Language { get; set; }
+    }
+}
