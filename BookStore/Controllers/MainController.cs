6c92cc7bb3e010eca97178bb5d88d76e5e637293
JONAS-PC\Jonas <100jonas@gmail.com>
2018-08-19 04:22:10 +0300

    Reimplementing update

--- BookStore/Controllers/MainController.cs
+++ BookStore/Controllers/MainController.cs
@@ -37,15 +37,20 @@ namespace BookStore.Controllers
 
         public ActionResult CreateBook(Book book)
         {
-            if (book.Id == 0)
+            if (book.Id == 0) //create
             {
                 book.Language = _context.Languages.Single(b => b.Id == book.LanguageId);
                 _context.Books.Add(book);
             }
-            else
+            else //update
             {
-                var bookInDb = _context.Books.SingleOrDefault(b => b.Id == book.Id);
-                TryUpdateModel(bookInDb);
+                var bookInDb = _context.Books.Include(b => b.Author).Include(b => b.Language).Single(b => b.Id == book.Id);
+                book.Language = _context.Languages.SingleOrDefault(b => b.Id == book.LanguageId);
+                book.Author.Id = book.AuthorId;
+                var a = _context.Books.Single(b => b.Id == book.Id);
+                book.Author.Books.Add(a);
+
+                UpdateModel(bookInDb);
             }
 
             _context.SaveChanges();
@@ -57,11 +62,16 @@ namespace BookStore.Controllers
             var book = _context.Books.Include(b => b.Author).SingleOrDefault(b => b.Id == id);
 
             if (book == null)
-            {
                 HttpNotFound();
-            }
 
-            return View("AddBook", book);
+            BookView bookView = new BookView
+            {
+                Book = book,
+                Languages = _context.Languages.ToList()
+
+            };
+
+            return View("AddBook", bookView);
         }
     }
 }
