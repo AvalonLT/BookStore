807835e3f80b24dcd5392a1fc0e7619aabd4fc28
JONAS-PC\Jonas <100jonas@gmail.com>
2018-08-24 03:24:41 +0300

    API

---
+++ BookStore/Controllers/Api/BooksController.cs
@@ -0,0 +1,83 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookStore.Controllers.Api
+{
+    public class BooksController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public BooksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/books
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        //GET /api/Books
+        public Book GetBook(int id)
+        {
+            var book = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            if (book == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return book;
+        }
+
+        //POST /api/Books
+        [HttpPost]
+        public Book CreateBook (Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return book;
+        }
+
+        //PUT /api/Books
+        [HttpPut]
+        public void UpdateBook(Book book, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            if (bookInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            bookInDb.Author.Name = book.Author.Name;
+            bookInDb.Author.LastName = book.Author.LastName;
+            bookInDb.Hardcover = book.Hardcover;
+            bookInDb.ISBN = book.ISBN;
+            bookInDb.Language = book.Language;
+            bookInDb.LanguageId = book.LanguageId;
+            bookInDb.PageCount = book.PageCount;
+            bookInDb.Title = book.Title;
+            bookInDb.Year = book.Year;
+
+            _context.SaveChanges();
+        }
+    }
+}
